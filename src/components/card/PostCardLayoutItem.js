96b2187b0ac759e0d741223ab7ce6f8b7ebab75f
chang-hae <shootthemood919@gmail.com>
2022-09-25 02:12:35 +0900

    apply division padding

--- src/components/card/PostCardLayoutItem.js
+++ src/components/card/PostCardLayoutItem.js
@@ -2,36 +2,38 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    padding: 0.5rem 0;
+    padding: 1rem 0;
 `;
 
 export const FlexContainer = styled.div`
     display: flex;
     flex-direction: row;
-
 `;
 
 export const Datetime = styled.span`
+    width: 110px;
+    text-align: center;
     font-size: 1.1rem;
     font-weight: bold;
-    padding: 1rem;
 `;
 
 export const Division = styled.span`
+    width: 80px;
+    text-align: center;
     font-size: 1.1rem;
     font-weight: bold;
-    padding: 1rem;
 `;
 
 export const Title = styled.span`
     font-size: 1.5rem;
-    padding: 1rem;
+    padding-left: 0.5rem;
 `;
 
 export const Wall = styled.span`
     font-size: 1.1rem;
     font-weight: bold;
-    padding: 1rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
 `
 
 export const StyledLink = styled(Link)`
