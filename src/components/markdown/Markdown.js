ca1323b452ed773fd809a82b52021c63927e7aba
chang-hae <shootthemood919@gmail.com>
2022-09-25 23:20:55 +0900

    markdown image align set

--- src/components/markdown/Markdown.js
+++ src/components/markdown/Markdown.js
@@ -2,30 +2,36 @@ import * as React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { nord } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { darcula } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const code = ({ node, inline, className, children, ...props }) => {
-    return inline ? (
-        // 강조 (``)
-        <code
-            style={{
-                background: "linear-gradient(to top, #FFE400 15%, transparent 15%)",
-                padding: "4px 2px",
-            }}
-            {...props}
-        >
-            {children}
-        </code>
-    ) : (
-        // 코드 (```)
+    if (inline) {
+        return (
+            <code
+                style={{
+                    background: "linear-gradient(to top, #FFE400 15%, transparent 15%)",
+                    padding: "4px 2px",
+                }}
+                {...props}
+            >
+                {children}
+            </code>
+        )
+    }
+
+    const match = /language-(\w+)/.exec(className || '');
+    return match ? (
         <SyntaxHighlighter
-            style={nord}
-            language="javascript"
+            children={String(children).replace(/\n$/, '')}
+            style={darcula}
+            language={match[1]}
             PreTag="div"
             {...props}
-        >
-            {String(children).replace(/\n$/, "")}
-        </SyntaxHighlighter>
+        />
+    ) : (
+        <code className={className} {...props}>
+            {children}
+        </code>
     );
 };
 
@@ -49,9 +55,9 @@ const blockquote = ({ node, children, ...props }) => {
 const img = ({ node, ...props }) => {
     return (
         <img
-            style={{ maxWidth: "60vw" }}
+            style={{ maxWidth: "100%" }}
             src={props.src.replace("../../../../public/", "/")}
-            alt="MarkdownRenderer__Image"
+            alt={props.alt}
         />
     );
 }
@@ -72,11 +78,28 @@ const a = ({ node, children, ...props }) => {
     );
 };
 
+const p = ({ node, children, ...props }) => {
+    const hasImage = node.children.filter(child => child.tagName === "img").pop();
+    if(!!hasImage) {
+        return (
+            <p align="center" {...props}>
+                {children}
+            </p>
+        );
+    }
+
+    return (
+        <p style={{ fontSize: "1.2rem" }} {...props}>
+            {children}
+        </p>
+    );
+};
+
 const Markdown = ({ content }) => {
     return (
         <ReactMarkdown
             remarkPlugins={[remarkGfm]}
-            components={{ code, blockquote, img, em, a }}>
+            components={{ code, blockquote, img, em, a, p }}>
             {content}
         </ReactMarkdown>
     )
--- src/pages/1.js
+++ src/pages/1.js
@@ -5,13 +5,37 @@ import { postState } from '../store/atom';
 import Markdown from '../components/markdown/Markdown';
 
 const content = `
-[material-ui](https://mui.com/core/ "material ui link") 를 써보면 ThemeProvider 가 제공되는데요.
+### 1. React 에서 Context 가 뭡니까용?
 
-### 1. Context 가 뭡니까용?
+![react component tree](/pages/1/1.jpg)
 
-### 2. Theme.js 만들기
+리액트는 위의 예시처럼 컴포넌트 트리 구조로 동작하는데요.
 
-\`\`\`
+상위 컴포넌트에서 하위 컴포넌트를 호출하는 *Top-Down* 방식이예요.
+
+이때 상위 컴포넌트에서 하위 컴포넌트에 넘겨주는 데이터를 **\`Props\`** 라고 해요.
+
+그런데 만약에 *App.js* 에서 *Gen2* 컴포넌트로 Props 를 내려주고 싶다면 중간 컴포넌트에서 이를 계속 전달해줘야하는 불편함이 있어요.
+
+이러한 불편을 해결해줄 수 있는 기능이 바로 **\`Context\`** 예요.
+
+**\`ContextProvider\`** 로 *App.js* 에서 호출하는 하위 컴포넌트를 감싸주면 하부 컴포넌트 트리 전체에서 *Context* 가 제공하는 데이터를 편리하게 가져다 쓸 수 있어요.
+
+
+
+### 2. material-ui 에서 제공되는 ThemeProvider
+
+*[material-ui](https://mui.com/core/ "material ui link")* 를 써보면 내장 구현된 **\`ThemeProvider\`** 가 제공되는데요.
+
+이 부분에 착안해서 **\`createContext\`** 를 활용한 *ThemeProvider* 를 한 번 만들어 볼께요.
+
+![smile image](/pages/1/2.jpg "option")
+
+롸쓰고우!
+
+### 3. Theme.js 만들기
+
+\`\`\`js
 import * as React from 'react';
 import { createContext } from 'react';
 
@@ -32,96 +56,21 @@ const ThemeContextProvider = ({ children }) => {
 export { ThemeContextProvider, ThemeContext };
 \`\`\`
 
+먼저 *Theme.js* 파일을 만들고 위와 같이 코드를 작성해주세요.
 
+코드를 보면 *createContext* 를 이용해서 ThemeContext 를 생성해주는데요.
 
-### 3. ThemeProvider 적용하기
+이 객체가 바로 하부 트리 컴포넌트에서 가져다 쓸 컨텍스트예요.
 
-***
+일종의 저장소라 생각하시면 되고 여기 저장소에 *primary* 프로퍼티와 그 값이 저장되어 있는거죠.
 
-### 4. Theme Context 값 사용하기
+자, 이렇게 해서 컨텍스트 프로바이더 생성은 끝났어요.
 
+이제 컴포넌트에서 사용할 수 있도록 적용해볼께요.
 
+### 4. ThemeProvider 적용하기
 
-
-\`\`\`
-    String a = "aaa";
-    String b = "bbb";
-    log.info(a + b);
-\`\`\`
-
-
-글자 \`
-String a = "aaa";
-String b = "bbb";
-log.info(a + b);
-\`
-
-> This is a first blockqute.
->	> This is a second blockqute.
->	>	> This is a third blockqute.
-
-1. 첫번째
-2. 두번째
-3. 세번째
-
-1. 첫번째
-3. 세번째
-2. 두번째
-
-* 빨강
-  * 녹색
-    * 파랑
-
-+ 빨강
-  + 녹색
-    + 파랑
-
-- 빨강
-  - 녹색
-    - 파랑
-
-* * *
-
-***
-    
-*****
-    
-- - -
-    
----------------------------------------
-
-[link keyword][id]
-
-[id]: URL "Optional Title here"
-
-// code
-Link: [Google][googlelink]
-
-[googlelink]: https://google.com "Go google"
-
-사용문법: [Title](link)
-적용예: [Google](https://google.com, "google link")
-
-일반적인 URL 혹은 이메일주소인 경우 적절한 형식으로 링크를 형성한다.
-
-* 외부링크: <http://example.com/>
-* 이메일링크: <address@example.com>
-
-single asterisks   
-*single asterisks*   
-_single underscores_   
-**double asterisks**   
-__double underscores__   
-~~cancelline~~   
-
-![Alt text](https://images.unsplash.com/photo-1662914387418-3b93b1fd53db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)
-![Alt text](https://images.unsplash.com/photo-1662914387418-3b93b1fd53db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80 "Optional title")
-
-* 줄 바꿈을 하기 위해서는 문장 마지막에서 3칸이상을 띄어쓰기해야 한다.   
-이렇게
-
-* 줄 바꿈을 하기 위해서는 문장 마지막에서 3칸이상을 띄어쓰기해야 한다.___\\ 띄어쓰기
-이렇게
+### 5. Theme Context 값 사용하기
 `;
 
 const Page1 = () => {
